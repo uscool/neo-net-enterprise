afc23c258a3a6dea2688e1dbdc2d88312a71ceed
uscool <106803159+uscool@users.noreply.github.com>
2025-02-16 11:09:05 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,27 +1,41 @@
-from google import genai
+import tkinter as tk
+from tkinter import ttk, scrolledtext
 from dotenv import load_dotenv
 import os
+from google import genai
 
-load_dotenv("auth.env")
-api_key = os.getenv("GOOGLE_API_KEY")
+def load_api_key():
+    load_dotenv("auth.env")
+    return os.getenv("GOOGLE_API_KEY")
 
-client = genai.Client(api_key=api_key)
+client = genai.Client(api_key=load_api_key())
 
 def finding_hashtags(industry):
-    response = client.models.generate_content(
+    response=client.models.generate_content(
         model="gemini-2.0-flash",
-        contents=[f"generate a list of 10 trending social media hashtags related to the {industry} industry. I need only hashtags as output, no explanation, nothing else."]
+        contents=[f"generate a list of 10 trending social media hashtags related to the {industry} industry. I need only hashtags as output no explanation, nothing else."]
+
     )
     if response.text:
         hashtags = response.text.split("\n")
         return [tag.strip() for tag in hashtags if tag.startswith("#")]
     else:
         return ["No hashtags found."]
+    
+def suggestions(hashtags):
+    response=client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=[f"I'm a company who wants to use these hashtags for advertising: {', '.join(hashtags)} give me recommendations on how to use them."]
+
+    )
+    print("\nMarketing Recommendations:\n")
+    print(response.text)
 
 def main():
-    industry = input("Enter industry or related advert term: ")
+    industry = input("Enter industry or advert term: ")
     hashtags = finding_hashtags(industry)
     print("\nTrending Hashtags are:")
     print(", ".join(hashtags))
+    suggestions(hashtags)
 
 main()
